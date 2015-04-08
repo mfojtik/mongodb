389faace75809bb93162bc3f3c2bae4d27042d24
christoph Görn <goern@redhat.com>
2015-04-08 14:01:00 +0200

    added atomic tool glue

---
+++ 2.4/root/usr/bin/install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Make Dirs
+
+mkdir -p ${HOST}/${CONFDIR} ${HOST}/${LOGDIR}/mongodb ${HOST}/${DATADIR}
---
+++ 2.4/root/usr/bin/uninstall.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rm -rf ${HOST}/${CONFDIR} ${HOST}/${LOGDIR}/mongodb ${HOST}/${DATADIR} 
